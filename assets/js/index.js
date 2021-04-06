cc654f4dfc28e4f64ef97a9bbc97d639b733d5a6
DovzhenkoDenys1989 <ddv10jan1989@gmail.com>
2021-04-06 11:41:20 +0300

    Add arrow func example

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,84 +1,21 @@
 'use strict';
 
-const subscribe = function(elem){
-  elem.isSubscribed = true;
+function test(){
+  console.log(this);
 }
 
-
-function square(currentNumber) {
-  const result = currentNumber * currentNumber;
-  console.log(result);
-  return result;
-
+const test2 = function(){
+  console.log(this);
 }
 
-function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min; 
+const test3 = () => {
+  console.log(this);
 }
 
-const numsArr = [];
-for(let i = 0; i < 10; i++){
-  numsArr.push(getRandomIntInclusive(100, 200));
+test();
+test2();
+test3();
+
+const obj = {
+  test: test,
 }
-
-function User(firstName, lastName, email, age, nationality, isMale, isSubscribed = false){
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.email = email;
-  this.age = age;
-  this.nationality = nationality;
-  this.isMale = isMale;
-  this.isSubscribed = isSubscribed;
-}
-
-User.prototype = new UserProto();//1.
-
-function UserProto(){ //1.
-this.getFullName = function(){
-  return `${this.firstName} ${this.lastName}` //2.
-}
-}
-
-/*
-1. Создать прототип для User.
-2. Создать ф-ю getFullName, которая вернёт строку с именем и фамилией пользователя
-3. Добавить рандомную генерацию подписок (значение поля isSubscribed)
-4. Получить массив только подписанных совершеннолетних
-пользователей женского пола. (Array.prototype.filter)
-*/
-
-function getRandomUsers(amount){
-  const usersArray = [];
-  for(let i = 0; i < amount; i++){
-    usersArray.push(
-      new User(
-        `Name${i}`, //firstName
-        `Surname${i}`,//lastName
-        `user${i}@gmail.com`,//email
-        getRandomIntInclusive(10, 50),//age
-        getRandomNationality(),//nationality
-        Math.random() < 0.5,//isMale
-        Math.random() < 0.5,//isSubscribed //3.
-
-      )
-    );
-  }
-  return usersArray;
-}
-
-function getRandomNationality(){
- return NATIONALITIES[getRandomIntInclusive(0, NATIONALITIES.length - 1)];
-}
-
-const usersList = getRandomUsers(100);//4.
-
-function getSubscribedAdultWomen(arr){//4.
-return arr.filter(function(user){
-  return user.age >=18 && !user.isMale && user.isSubscribed && user.nationality === 'ua';
-});
-}
-
-
-
